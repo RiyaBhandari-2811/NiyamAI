07cb33673b7c43d33a8c612875febb58e1b94418
Riya <bhandari28riya@gmail.com>
2025-10-31 15:02:08 +0530

    added creds

--- niyam-ai-web/src/config/getGoogleSecret.ts
+++ niyam-ai-web/src/config/getGoogleSecret.ts
@@ -1,6 +1,7 @@
 import { SecretManagerServiceClient } from "@google-cloud/secret-manager";
 
-const client = new SecretManagerServiceClient();
+const credentials = JSON.parse(process.env.GOOGLE_APPLICATION_CREDENTIALS_JSON as string);
+const client = new SecretManagerServiceClient({ credentials });
 
 export async function getAesKey() {
   const [version] = await client.accessSecretVersion({
